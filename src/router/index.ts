5d2788db0527327a2129fc1b38a7d8fb8814729e
HasegawaTakatune <0ta5274738t4p4r@gmail.com>
2023-02-13 23:57:17 +0900

    ビルドコミット

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/page1',
+      path: '/',
       name: 'page1',
       component: () => import('../views/Samples/Page1.vue')
     },
